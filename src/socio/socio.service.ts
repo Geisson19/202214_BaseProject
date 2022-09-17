d21cd106e30ba54972384eeee40e9008cebb286a
Geisson19 <gs.ponce20@uniandes.edu.co>
2022-09-17 16:28:21 -0500

    entities documentation

--- src/socio/socio.service.ts
+++ src/socio/socio.service.ts
@@ -13,10 +13,20 @@ export class SocioService {
     private readonly socioRepository: Repository<Socio>,
   ) {}
 
+  /**
+   * It returns true if the email address of the given socio contains an @ symbol
+   * @param {Socio} socio - Socio - The object that we're validating.
+   * @returns A boolean value.
+   */
   isValidEmail(socio: Socio) {
     return socio.correo.includes('@');
   }
 
+  /**
+   * If the email is valid, save the socio
+   * @param {Socio} socio - Socio - this is the object that we're going to save to the database.
+   * @returns The saved socio.
+   */
   async create(socio: Socio) {
     if (!this.isValidEmail(socio)) {
       return;
@@ -24,10 +34,19 @@ export class SocioService {
     return await this.socioRepository.save(socio);
   }
 
+  /**
+   * It returns a list of all the socios in the database, including the club they belong to
+   * @returns An array of Socio objects.
+   */
   async findAll() {
     return await this.socioRepository.find({ relations: ['club'] });
   }
 
+  /**
+   * It finds a socio by its id and returns it
+   * @param {string} id - string - The id of the socio we want to find
+   * @returns A socio object
+   */
   async findOne(id: string) {
     const socio: Socio = await this.socioRepository.findOne({
       where: { id },
@@ -42,6 +61,12 @@ export class SocioService {
     return socio;
   }
 
+  /**
+   * It updates a socio in the database
+   * @param {string} id - The id of the socio to update
+   * @param {Socio} socio - Socio - The socio object that we want to update.
+   * @returns The updated socio
+   */
   async update(id: string, socio: Socio) {
     const persistedSocio: Socio = await this.socioRepository.findOne({
       where: { id },
@@ -63,6 +88,10 @@ export class SocioService {
     });
   }
 
+  /**
+   * It deletes a socio from the database
+   * @param {string} id - string - The id of the socio to be deleted
+   */
   async delete(id: string) {
     const socio: Socio = await this.socioRepository.findOne({
       where: { id },
