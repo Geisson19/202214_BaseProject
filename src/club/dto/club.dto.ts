7600c5e693bca67abf53f763bff04405b95fbb8d
Geisson19 <gs.ponce20@uniandes.edu.co>
2022-09-17 20:48:38 -0500

    DTOS

--- src/club/dto/club.dto.ts
+++ src/club/dto/club.dto.ts
@@ -1,1 +1,27 @@
-export class ClubDto {}
+import {
+  IsString,
+  IsNotEmpty,
+  IsDateString,
+  IsUrl,
+  Length,
+} from 'class-validator';
+
+export class ClubDto {
+  @IsString()
+  @IsNotEmpty()
+  nombre: string;
+
+  @IsNotEmpty()
+  @IsDateString()
+  fechaFundacion: Date;
+
+  @IsString()
+  @IsNotEmpty()
+  @IsUrl()
+  imageUrl: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @Length(0, 100)
+  descripcion: string;
+}
--- src/socio/dto/socio.dto.ts
+++ src/socio/dto/socio.dto.ts
@@ -1,1 +1,16 @@
-export class SocioDto {}
+import { IsString, IsNotEmpty, IsDateString, IsEmail } from 'class-validator';
+
+export class SocioDto {
+  @IsString()
+  @IsNotEmpty()
+  nombre: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @IsEmail()
+  correo: string;
+
+  @IsDateString()
+  @IsNotEmpty()
+  fechaNacimiento: Date;
+}
